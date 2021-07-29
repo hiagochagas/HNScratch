847671eb70a6c103c133ecd2ca549e76c4fc26b0
Hiago <hiago.chagas.14@gmail.com>
2021-07-29 08:17:23 -0300

    Initial Commit

---
+++ HNScratch/HNScratch.h
@@ -0,0 +1,18 @@
+//
+//  HNScratch.h
+//  HNScratch
+//
+//  Created by Hiago Chagas on 29/07/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HNScratch.
+FOUNDATION_EXPORT double HNScratchVersionNumber;
+
+//! Project version string for HNScratch.
+FOUNDATION_EXPORT const unsigned char HNScratchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HNScratch/PublicHeader.h>
+
+
